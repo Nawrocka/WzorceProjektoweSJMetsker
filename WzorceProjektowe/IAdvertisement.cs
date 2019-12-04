fb4b0566bed1e6bb742fab7307975b7a4703d805
Nawrocka <wrocka.mm@gmail.com>
2019-12-04 12:39:57 +0100

    Practice interface implementation & expression body definition

---
+++ WzorceProjektowe/IAdvertisement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WzorceProjektowe
+{
+    interface IAdvertisement
+    {
+        int ID { get; }
+        string AdCopy { get; set; }
+
+        //above implementation is using properties thanks to which unknown client can have access to this properts through reflections
+        //don't use below type of implementation
+        //int GetID();
+        //string GetAdCopy();
+        //void SetAdCopy(string text);
+    }
+}
---
+++ WzorceProjektowe/WebAd.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WzorceProjektowe
+{
+    public class WebAd : IAdvertisement
+    {
+        private int _id;
+        private string _adCopy = "";
+        public WebAd(int id)
+        {
+            _id = id;
+        }
+
+        public int ID
+        {
+            get { return _id; }
+        }
+        public string AdCopy { get => _adCopy; set => _adCopy = value; } //expression body definition
+    }
+}
